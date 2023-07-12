6902177464768335e0be5e6f4ac24bdb026d24f0
Raymond <raymond.w.lang@gmail.com>
2023-07-12 00:17:00 -0700

    added route for a specific trip using id

--- server/routes/trip.js
+++ server/routes/trip.js
@@ -36,3 +36,52 @@ router.get(
       });
   }
 );
+
+// Get a specific trip.
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Trip.findById(req.params.id)
+      .populate({
+        path: "users",
+        model: "User",
+        select: ["username", "_id"],
+      })
+      .populate({
+        path: "comments",
+        model: "Comment",
+        select: ["author", "comment", "date"],
+      })
+      .populate({
+        path: "itineraryItems",
+        model: "ItineraryItem",
+      })
+      .populate({
+        path: "flightItineraryItems",
+        model: "FlightItineraryItem",
+      })
+      .populate({
+        path: "lodgingItineraryItems",
+        model: "LodgingItineraryItem",
+      })
+      .populate({
+        path: "foodItineraryItems",
+        model: "FoodItineraryItem",
+      })
+      .then((trip) => {
+        // Check if the current user is part of the trip.
+        if (trip.users.some((user) => req.user.id === user.id)) {
+          return res.json({ [trip.id]: trip });
+        } else {
+          // This user isn't authorized to view this trip.
+          return res
+            .status(401)
+            .json({ unauthorized: "You are not authorized" });
+        }
+      })
+      .catch((err) => {
+        return res.status(404).json({ notripfound: "This trip doesn't exist" });
+      });
+  }
+);
