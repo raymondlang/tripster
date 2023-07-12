32c44aaba50df137d88d9bdcd8bd04ea71b84ac7
Raymond <raymond.w.lang@gmail.com>
2023-07-12 09:48:45 -0700

    added flight itinerary util

---
+++ src/util/flight_itinerary_item_api_util.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const fetchAllFlightItineraryItems = () => {
+  return axios.get(`/api/flightitineraryitems/`);
+};
+export const fetchItineraryItem = (itemId) => {
+  return axios.get(`/api/flightitineraryitems/${itemId}`);
+};
+
+export const createItineraryItem = (data) => {
+  return axios.post(`/api/trips/${data.tripId}/flightItineraryItem`, data);
+};
+
+// export const updateItineraryItem = (data) => {
+//     return axios.patch(`/api/flightitineraryitems/${data.id}`, data);
+// }
+
+export const deleteItineraryItem = (itemId) => {
+  return axios.delete(`/api/trips/flightItineraryItems/${itemId}`);
+};
