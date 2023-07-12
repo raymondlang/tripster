34be4b9dc50e79d9659b5783936f52937bbfec1d
Raymond <raymond.w.lang@gmail.com>
2023-07-12 00:16:15 -0700

    set up get trips route

--- server/routes/trip.js
+++ server/routes/trip.js
@@ -15,3 +15,24 @@ import ValidateTripInput from "../../validation/trip";
 import ValidateCommentInput from "../../validation/comment";
 import validateItineraryItemInput from "../../validation/itineraryItem";
 import validText from "../../validation/valid-text";
+
+// Get the trips for a specific user.
+router.get(
+  "/user/:user_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.id !== req.params.user_id) {
+      return res.status(401).json({ unauthorized: "You are not authorized" });
+    }
+
+    // Finds all of the trips that have :user_id in their "users" array.
+    Trip.find({ users: { $all: [req.params.user_id] } })
+      .sort({ dat: -1 })
+      .then((trips) => res.json(trips))
+      .catch((err) => {
+        return res.status(404).json({
+          notripsfound: "This user doesn't have any trips",
+        });
+      });
+  }
+);
