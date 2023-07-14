ea5a9b675a537ac54e72fd48358dba0657b6cdbb
Raymond <raymond.w.lang@gmail.com>
2023-07-14 07:32:35 -0700

    fixed itinerary item

--- src/components/itinerary_items/ItineraryItem.jsx
+++ src/components/itinerary_items/ItineraryItem.jsx
@@ -1,5 +1,5 @@
 import ItineraryItemDetails from "./itinerary_item_details";
-import CreateItineraryItemContainer from "./create_itinerary_container";
+import CreateItineraryItem from "./flight/CreateItineraryItem";
 
 const ItineraryItem = (props) => {
   const itemsList = props.itineraryItems.map((item, idx) => (
